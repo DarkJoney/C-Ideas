055e9bc1e2d84ac82aa4f2f8e61dba441695f93a
Dmitriy <30622552+DarkJoney@users.noreply.github.com>
2019-03-25 18:19:04 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            int i = 0;
+            var counters = new List<PerformanceCounter>();
+
+            foreach (Process process in processes)
+            {
+                
+                var counter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName);
+                counter.NextValue();
+                counters.Add(counter);          
+            }
+
+          
+
+            Thread.Sleep(1000);
+
+            foreach (var counter in counters)
+            {
+                
+                if (counter.NextValue() > 0.50)
+                {
+                    Console.WriteLine(processes[i].ProcessName + " " + "Warning! " + "at ");
+                    try
+                    {
+                        string fullPath = processes[i].MainModule.FileName;
+                        Console.WriteLine(fullPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("ERROR---" + ex.Message);
+                    }
+
+                    Console.WriteLine("-------------------");
+
+
+                }
+                ++i;
+            }
+            Console.ReadLine();
+        } 
+    }
+}
